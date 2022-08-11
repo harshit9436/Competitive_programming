3659649557f6c82598377a8405429e3f305a9042
harshit9436 <harshitkumar.9436@gmail.com>
2022-08-11 21:29:26 +0530

    Aug 11

--- Codeforces/Double Strings.cpp
+++ Codeforces/Double Strings.cpp
@@ -84,8 +84,8 @@ void solve(vector<string> & arr){
 int main(){
 
     #ifndef ONLINE_JUDGE
-        freopen("input.txt" , "r" , stdin);
-        freopen("output.txt" , "w" , stdout);
+        freopen("zinput.txt" , "r" , stdin);
+        freopen("zoutput.txt" , "w" , stdout);
     #endif
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
