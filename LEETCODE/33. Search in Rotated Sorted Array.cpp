d242c29987fae97abc4c5bcde926496712893d9d
harshit <harshitkumar.9436@gmail.com>
2022-10-15 00:03:13 +0530

    15th Oct

---
+++ LEETCODE/33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,24 @@
+int search(vector<int>& nums, int target) {
+    int l =0, r= nums.size()-1;
+    while(l<=r){
+        cout<< l << " " << r<< endl;
+        int mid = (l+r)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(nums[l]<= nums[mid]){
+            if(target <= nums[mid]&& target >= nums[l]){
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }else{
+            if(target <= nums[r] &&target >= nums[mid] ){
+                l= mid+1;
+            }else{
+                r= mid-1;
+            }
+        }
+    }
+    return -1;
+}
